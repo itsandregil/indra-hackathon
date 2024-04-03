0c88e356bb0235284d8d544e2e28d183663136c3
Andre Gil <thomasgil913@gmail.com>
2024-04-03 08:58:02 -0500

    Pruebas de modelos de boosting con tratamiento de las fechas, transformacion logaritmica y escalamiento

--- utils.py
+++ utils.py
@@ -2,6 +2,19 @@
 from sklearn.base import BaseEstimator, TransformerMixin # To create custom transformers
 import numpy as np
 import pandas as pd
+from typing import Union
+
+
+def _check_X(X: Union[pd.DataFrame, np.ndarray, np.generic]) -> pd.DataFrame:
+    """Revisa que tipo de estrutura se pasa a los encoders
+    y convierte los datos a un DataFrame."""
+    if isinstance(X, (np.ndarray, np.generic)):
+        X = pd.DataFrame(X)
+    
+    if isinstance(X, pd.DataFrame):
+        X = X.copy()
+    
+    return X
 
 
 class FrecuencyEncoder(BaseEstimator, TransformerMixin):
@@ -11,6 +24,7 @@ class FrecuencyEncoder(BaseEstimator, TransformerMixin):
 
 def apply_base_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     """Aplica el preprocesamiento base definido por el equipo"""
+
     new_df = df.drop(['id_colaborador', 'fecha_nacimiento', 'fecha_incorporacion'], axis=1)
 
     # preprocess each column, imputing using 0 and
